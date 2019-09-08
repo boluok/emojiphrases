725fe9e15b7aab421d747bb25d31964f448270eb
Bolu Okunaiya <boluokunaiya@gmail.com>
2019-09-09 00:33:01 +0100

    hello

--- src/Application.kt
+++ src/Application.kt
@@ -24,9 +24,7 @@ import java.util.concurrent.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
 
     install(DefaultHeaders)
 
